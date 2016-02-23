c9c79c254b6b3bcf108b908c1ee0f39ea4f8a3de
Pat McGuire <pmcguire@solstice-mobile.com>
2016-02-23 10:05:01 -0600

    Adding eChallenge#2

---
+++ eChallenge2.playground/Contents.swift
@@ -0,0 +1,42 @@
+
+
+var topNum = 1000
+var mult3 = 0
+var mult5 = 0
+var multiples3: [Int] = []
+var multiples5: [Int] = []
+
+var answer = 0
+
+
+while true{
+    mult3 = mult3 + 3
+    
+    if mult3%5 != 0 {
+        multiples3.append(mult3)
+    }
+    if mult3 + 3 >= topNum{
+        break
+    }
+}
+
+while true{
+    mult5 = mult5 + 5
+    multiples5.append(mult5)
+    
+    if mult5 + 5 >= topNum{
+        break
+    }
+}
+
+
+for var i = 0; i < multiples3.count; ++i{
+    answer = answer + multiples3[i]
+}
+
+for var i = 0; i < multiples5.count; ++i{
+    answer = answer + multiples5[i]
+}
+
+print(answer)
+
